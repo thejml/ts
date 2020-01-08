41748eb7e05d2237d975c5869ab725aef942fce4
Joe Legeckis <jlegeckis@costar.com>
2020-01-08 16:26:01 -0500

    Now with optional color

--- ts.go
+++ ts.go
@@ -4,13 +4,35 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+    "flag"
 	"time"
+
+    "github.com/logrusorgru/aurora"
 )
+
+var slowMS *int
+var useColor *bool
+var lineTime *bool
+
+func init() {
+    useColor = flag.Bool("c", false, "Use Color")
+    slowMS = flag.Int("s", 2, "If using color, Time in MS to color slow requests in Red")
+    lineTime = flag.Bool("t", false, "Show time between lines")
+	flag.Parse()
+}
 
 func main() {
     var ts time.Time
     var lastStamp time.Time
+    var timeOut string
+    var au aurora.Aurora
+    au = aurora.NewAurora(*useColor)
 
+/*    if *useColor {
+        errTime := *slowMS 
+        warnTime:= (*slowMS/2)
+    }
+*/
     info, err := os.Stdin.Stat()
 	if err != nil {
 		panic(err)
@@ -27,8 +49,14 @@ func main() {
 	// Read in from the pipe until we can't.
 	for reader.Scan() {
 		input := reader.Text()
-        ts=time.Now()
-        fmt.Printf("%s (%7s): %s\n",ts.Format(time.StampMilli),ts.Sub(lastStamp).Truncate(time.Millisecond),input)
-        lastStamp=ts
+        if *lineTime {
+            ts=time.Now()
+            timeOut=fmt.Sprintf("(%7s): ",ts.Sub(lastStamp).Truncate(time.Millisecond))
+            fmt.Printf("%s %s %s\n",au.BrightBlack(ts.Format(time.StampMilli)),au.Green(timeOut),input) 
+            lastStamp=ts
+        } else {
+            ts=time.Now()
+            fmt.Printf("%s %s\n",au.BrightBlack(ts.Format(time.StampMilli)),input) 
+        }
     }
 }
